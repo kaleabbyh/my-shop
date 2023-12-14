42d20d1c102c20df15e69e241d79e9034e026e29
Kaleab Bayih <kaleabbyh@gmail.com>
2023-12-14 09:43:19 +0300

    Update c_is_fun.c

--- c/c_is_fun.c
+++ c/c_is_fun.c
@@ -24,3 +24,26 @@ func main() {
 
 	fmt.Println(string(body))
 }
+
+
+
+
+
+func dec() {
+	url := "https://api.example.com/data"
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("An error occurred:", err)
+		return
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("An error occurred:", err)
+		return
+	}
+
+	fmt.Println(string(body))
+}
