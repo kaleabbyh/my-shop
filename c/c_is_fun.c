bd2bdb6224429d80ae555301bd03ece3b16a62b3
Kaleab Bayih <kaleabbyh@gmail.com>
2023-12-20 14:49:46 +0300

    Update c_is_fun.c

--- c/c_is_fun.c
+++ c/c_is_fun.c
@@ -1,1 +1,57 @@
 
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	url := "https://api.ksv.com/data"
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("An error occurred:", err)
+		return
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("An error occurred:", err)
+		return
+	}
+
+	fmt.Println(string(body))
+}
+
+
+
+
+
+func dec() {
+	url := "https://api.example.com/data"
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("An error occurred:", err)
+		return
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("An error occurred:", err)
+		return
+	}
+
+	fmt.Println(string(body))
+}
+
+
+
+
+
+
