9023e99b9b63f3380a229c30a508d3afeae2165f
habjk <kaleabbyh@gmail.com>
2024-01-11 08:56:45 +0300

    update****

--- c/c_is_fun.c
+++ c/c_is_fun.c
@@ -65,7 +65,22 @@ func dec() {
 
 	fmt.Println(string(body))
 
-	
+	url := "https://api.example.com/data"
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("An error occurred:", err)
+		return
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("An error occurred:", err)
+		return
+	}
+
+	fmt.Println(string(body))
 }
 
 func Encode() {
